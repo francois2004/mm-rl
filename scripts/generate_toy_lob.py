67edc73e998cd24fccbc440c304211b61f4cc17d
François Rongeot <francois.rongeot4@gmail.com>
2026-02-14 17:25:36 +0100

    add toy lob data generator

---
+++ scripts/generate_toy_lob.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+np.random.seed(42)
+
+N = 10000
+dt = 1/254
+
+mu = 0. ##
+sigma = 0.02
+mid = [100]
+
+for i in range (N-1):
+    dS = mu*mid[-1]*dt + sigma*mid[-1]*np.sqrt(dt)*np.random.randn()
+    mid.append(mid[-1]+dS)
+
+mid = np.array(mid)
+
+spread = .01
+bid = mid-spread/2
+ask = mid+spread/2
+
+bid_vol = np.random.poisson(10,N)
+ask_vol = np.random.poisson(10,N)
+
+df = pd.DataFrame({ "mid" : mid, "bid" : bid, "ask": ask, "bid_vol" : bid_vol, "ask_vol" :ask_vol})
+df.to_csv("data/raw/toy_lob.csv", index = False)
